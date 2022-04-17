bd0efa2363e5469df9cbf07ec9014837de8ca0a1
iynsys <irgiyansy@gmail.com>
2022-04-17 19:30:06 +0700

    Initial commit

---
+++ 2TipeDataNumber.php
@@ -0,0 +1,27 @@
+<?php
+   echo "Decimal : ";
+   var_dump(1234);
+   
+   echo "Octal : ";
+   var_dump(0123);
+   
+   echo "Hexadecimal : ";
+   var_dump(0x1A);
+
+   echo "Binary : ";
+   var_dump(0b11111111);
+
+   echo "Underscore di Number : ";
+   var_dump(1_2_3_4567);
+   
+   echo "Floating point : ";
+   var_dump(1.2356);
+
+   echo "Floating point dengan E notation Plus (1.2 x 1000) : ";
+   var_dump(1.2e3);
+
+   echo "Floating point dengan E notation minus (1.2 x 0.001) : ";
+   var_dump(1.2e-3);
+
+   echo "Underscore di floating point : ";
+   var_dump(1.2_3e3);
